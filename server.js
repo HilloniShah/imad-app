80b90f262e67aab446d83cc8c9b3b4f49b063e11
Hilloni Shah <hilloni98@gmail.com>
2017-08-20 17:29:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,21 +5,90 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+    title:'Article-one | Hilloni',
+    heading:'article-one',
+    date:'August 21, 2017',
+    content: ` <p>
+                    This is the content of my first artice. I am doing a course on Introduction to Mobile Application Development. I enjoy learning this course. This is the content of my first artice. I am doing a course on Introduction to Mobile Application Development. I enjoy learning this course. This is the content of my first artice. I am doing a course on Introduction to Mobile Application Development. I enjoy learning this course. This is the content of my first artice. I am doing a course on Introduction to Mobile Application Development. I enjoy learning this course. This is the content of my first artice. I am doing a course on Introduction to Mobile Application Development. I enjoy learning this course.
+                </p>
+                <p>
+                    This is the content of my first artice. I am doing a course on Introduction to Mobile Application Development. I enjoy learning this course. This is the content of my first artice. I am doing a course on Introduction to Mobile Application Development. I enjoy learning this course. This is the content of my first artice. I am doing a course on Introduction to Mobile Application Development. I enjoy learning this course. This is the content of my first artice. I am doing a course on Introduction to Mobile Application Development. I enjoy learning this course. This is the content of my first artice. I am doing a course on Introduction to Mobile Application Development. I enjoy learning this course.
+                </p>`
+    
+};
+
+var articleTwo={
+    title:'Article-Two | Hilloni',
+    heading:'article-two',
+    date:'August 22, 2017',
+    content: ` <p>
+                    This is the content of my second artice. I am doing a course on Introduction to Mobile Application Development. I enjoy learning this course. This is the content of my first artice. I am doing a course on Introduction to Mobile Application Development. I enjoy learning this course. This is the content of my first artice. 
+                </p>`
+    
+};
+
+var articleThree={
+    title:'Article-three | Hilloni',
+    heading:'article-three',
+    date:'August 23, 2017',
+    content: ` <p>
+                    This is the content of my third artice. I am doing a course on Introduction to Mobile Application Development. I enjoy learning this course. This is the content of my first artice. I am doing a course on Introduction to Mobile Application Development. I enjoy learning this course. This is the content of my first artice. I am doing a course on Introduction to Mobile Application Development. I enjoy learning this course. This is the content of my first artice. I am doing a course on Introduction to Mobile Application Development. I enjoy learning this course. This is the content of my first artice. I am doing a course on Introduction to Mobile Application Development. I enjoy learning this course.
+                </p>`
+    
+};
+
+function createTemplate(data){
+    
+        var heading=data.heading;
+        var title=data.title;
+        var date=data.date;
+        var conten=data.content;
+        
+        var htmlTemplate=`<html>
+            <head>
+                <title>
+                    ${heading}
+                </title>
+                <link href="/ui/style.css" rel="stylesheet"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            </head>
+            
+            <body>
+                
+                <div class="container">
+                    <div>
+                        <a href="/">Back to Home Page</a>
+                    </div>
+                    <hr/>
+                    <h2>${title} </h2>
+                    <div>
+                        ${date} 
+                    </div>
+                    <div>
+                       ${content}
+                    </div>
+                </div>
+            </body>
+        </html>`;
+        return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-})
+    res.sendFile(createTemplate(articleOne));
+});
 
 app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-})
+    res.sendFile(createTemplate(articleTwo));
+});
 
 app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-})
+    res.sendFile(createTemplate(articleThree));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));

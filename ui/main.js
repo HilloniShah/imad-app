86cb6e8b4d28c4a50f5e8958f627a64e50fb2e5b
Hilloni Shah <hilloni98@gmail.com>
2017-08-21 22:05:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,16 @@ console.log('Loaded!');
 
 var element=document.getElementById("main");
 element.innerHTML="Hilloni!!";
+
+var image=document.getElementById("main");
+
+var moveLeft=0;
+
+function moveRight(){
+    moveLeft=moveLeft+5;
+    img.style.marginLeft=marginLeft + 'px';
+}
+
+img.onclick=function(){
+    var int=setInterval(moveRight,50);
+}

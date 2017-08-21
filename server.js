881e5d2c8aa1dc3fa380b1943e3e63d54e7da946
Hilloni Shah <hilloni98@gmail.com>
2017-08-21 18:19:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ function createTemplate(data){
         var heading=data.heading;
         var title=data.title;
         var date=data.date;
-        var conten=data.content;
+        var content=data.content;
         
         var htmlTemplate=`<html>
             <head>
